233317c8f45d27034d2db4dbf716a8eb8db9724c
Arfarafar <andrew2001@bk.ru>
2022-11-08 14:14:58 +0300

    06-ext2-walk-path

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -1,10 +1,42 @@
 #include <solution.h>
 #include <ext2fs/ext2fs.h>
        #include <unistd.h>
+       #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
        #include <sys/stat.h>
 
+
+int dir_reader(int img, long int block_size, int upper_bound, uint32_t* blocks, const char* left_path, char entry_type){
+
+	char buf[block_size];
+
+	for (int i = 0; i < upper_bound; i++) {
+		if(blocks[i] == 0)
+			return -ENOENT;
+
+		if(pread(img, buf, block_size, block_size*blocks[i]) != block_size){
+			return -errno;
+		}
+		struct ext2_dir_entry_2* dir_entry = (struct ext2_dir_entry_2*) buf;
+
+		int remainsize = block_size;
+		
+		while (remainsize > 0){
+			remainsize -= dir_entry -> rec_len;
+			if(!strncmp(dir_entry -> name, left_path, dir_entry -> name_len)){
+				if(dir_entry -> file_type != entry_type)
+					return -ENOTDIR;
+				return dir_entry -> inode;
+			}
+
+			dir_entry = (struct ext2_dir_entry_2*) ((char*) (dir_entry) +  dir_entry -> rec_len);	
+		}
+		
+	}
+	
+	return 1;
+}
 
 
 int block_transfer(int img, int out, long int block_size, long long int* remainfilesize, int upper_bound, uint32_t* blocks){
@@ -29,15 +61,139 @@ int block_transfer(int img, int out, long int block_size, long long int* remainf
 
 
 
+long int inode_offset(int img, struct ext2_super_block* super_block, long int block_size, int inode_nr){
+
+	int addr_bg_descr = ((super_block -> s_first_data_block+1)*block_size + sizeof(struct ext2_group_desc)*((inode_nr-1) / super_block -> s_inodes_per_group));
+	struct ext2_group_desc group_desc = {};
+	if(pread(img, (char*)&group_desc, sizeof(struct ext2_group_desc), addr_bg_descr) != sizeof(struct ext2_group_desc))
+		return -errno;
+
+
+	return group_desc.bg_inode_table*block_size + ((inode_nr-1) % super_block -> s_inodes_per_group)*super_block -> s_inode_size;
+}
+
+
+int Find_ino(int img, struct ext2_super_block* super_block, long int block_size, int inode_nr, const char* path){
+
+	if (strlen(path) == 0){
+		return -ENOENT;
+	}
+
+	struct ext2_inode inode = {};
+	int offset = inode_offset(img, super_block, block_size, inode_nr);
+	if(offset < 0)
+		return offset;
+	if(pread(img, (char*)&inode, sizeof(struct ext2_inode), offset) != sizeof(struct ext2_inode))
+		return -errno;
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	uint32_t x1blocks[block_size/4];
+	uint32_t x2blocks[block_size/4];
+
+	char* ch = strchr(path, '/');
+	char entry_type = ch != NULL ? EXT2_FT_DIR : EXT2_FT_REG_FILE;
+	int entry_len = ch != NULL ? ch - path + 1 : (int)strlen(path);
+	
+	int res = dir_reader(img, block_size, EXT2_IND_BLOCK, inode.i_block, path, entry_type);
+	if(res <= 0)
+		return res;
+	if (res > 2)
+		return ch == NULL ? res : Find_ino(img, super_block, block_size, res, path + entry_len);
+//---------------------------------------------------------------------------------------------------------------------------------------	
+
+	if(inode.i_block[EXT2_IND_BLOCK] == 0){
+		return -ENOENT;
+	}
+	if(pread(img, (char*)x1blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK]) != block_size){
+		return -errno;
+	}
+	
+	res = dir_reader(img, block_size, block_size/4, x1blocks, path, entry_type);
+	
+	if(res <= 0)
+		return res;
+	if (res > 2)
+		return ch == NULL ? res : Find_ino(img, super_block, block_size, res, path + entry_len);
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+	if(inode.i_block[EXT2_IND_BLOCK + 1] == 0){
+		return -ENOENT;
+	}
+	if(pread(img, (char*)x2blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK+1]) != block_size){
+		return -errno;
+			
+	}
+
+	for (int j = 0; j < block_size/4; ++j)
+	{
+		if(pread(img, (char*)x1blocks, block_size, block_size * x2blocks[j]) != block_size){
+			return-errno;
+			
+		}
+			
+		res = dir_reader(img, block_size, block_size/4, x1blocks, path, entry_type);
+		if(res <= 0)
+			return res;
+		if (res > 2)
+			return ch == NULL ? res : Find_ino(img, super_block, block_size, res, path + entry_len);
+	}
+
+
+	 return -ENOENT;
+}
+
+
+int copy_file(int img, int out, struct ext2_super_block* super_block, long int block_size, int inode_nr){
+
+	struct ext2_inode inode = {};
+	int offset = inode_offset(img, super_block, block_size, inode_nr);
+	if(offset < 0)
+		return offset;
+	if(pread(img, (char*)&inode, sizeof(struct ext2_inode), offset) != sizeof(struct ext2_inode))
+		return -errno;
+
+	long long remainfilesize = ((long long)inode.i_size_high << 32L) + (long long)inode.i_size;
+	uint32_t x1blocks[block_size/4];
+	uint32_t x2blocks[block_size/4];
+
+	
+	int res = block_transfer(img, out, block_size, &remainfilesize, EXT2_IND_BLOCK, inode.i_block);
+	if(res <= 0)
+		return res;
+
+	if(pread(img, (char*)x1blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK]) != block_size){
+		return -errno;
+		
+	}
+	res = block_transfer(img, out, block_size, &remainfilesize, block_size/4, x1blocks);
+	if(res <= 0)
+		return res;
+
+
+	if(pread(img, (char*)x2blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK+1]) != block_size){
+		return -errno;
+	}
+
+	for (int j = 0; j < block_size/4; ++j)
+	{
+		if(pread(img, (char*)x1blocks, block_size, block_size * x2blocks[j]) != block_size)
+			return -errno;
+
+		res = block_transfer(img, out, block_size, &remainfilesize, block_size/4, x1blocks);
+		if(res <= 0)
+			return res;
+	}
+
+	return -E2BIG;
+}
+
+
+
+
 
 
 int dump_file(int img, const char *path, int out)
 {
-
-	struct stat statbuf = {};
-	if(stat(path, &statbuf) < 0)
-		return -errno;
-	int inode_nr = statbuf.st_ino;
 
 	struct ext2_super_block super_block = {};
 	if(pread(img, (char*)&super_block, sizeof(struct ext2_super_block), SUPERBLOCK_OFFSET) != sizeof(struct ext2_super_block))
@@ -45,55 +201,11 @@ int dump_file(int img, const char *path, int out)
 
 	long int block_size = 1024 << super_block.s_log_block_size;	
 	
-	int addr_bg_descr = ((super_block.s_first_data_block+1)*block_size + sizeof(struct ext2_group_desc)*((inode_nr-1) / super_block.s_inodes_per_group));
-	struct ext2_group_desc group_desc = {};
-	if(pread(img, (char*)&group_desc, sizeof(struct ext2_group_desc), addr_bg_descr) != sizeof(struct ext2_group_desc))
-		return -errno;
+	int inode_nr = EXT2_ROOT_INO;
 
-	struct ext2_inode inode = {};
-	if(pread(img, (char*)&inode, sizeof(struct ext2_inode), group_desc.bg_inode_table*block_size + ((inode_nr-1) % super_block.s_inodes_per_group)*super_block.s_inode_size) != sizeof(struct ext2_inode))
-		return -errno;
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------
-	long long remainfilesize = ((long long)inode.i_size_high << 32L) + (long long)inode.i_size;
-	//printf("%Ld\n", remainfilesize);
-	//printf("%ld %d\n", sizeof(struct ext2_inode), super_block.s_inode_size);
-	uint32_t* x1blocks = (uint32_t*)malloc(block_size);
-	uint32_t* x2blocks = (uint32_t*)malloc(block_size);
+	inode_nr = Find_ino(img, &super_block, block_size, inode_nr, path);
+	if (inode_nr < 0)
+		return inode_nr;
 
-	
-	int res = block_transfer(img, out, block_size, &remainfilesize, EXT2_IND_BLOCK, inode.i_block);
-	if(res <= 0)
-		goto out;
-//---------------------------------------------------------------------------------------------------------------------------------------	
-
-	if(pread(img, (char*)x1blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK]) != block_size){
-		res = -errno;
-			goto out;
-	}
-	res = block_transfer(img, out, block_size, &remainfilesize, block_size/4, x1blocks);
-	if(res <= 0)
-		goto out;
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-
-	if(pread(img, (char*)x2blocks, block_size, block_size * inode.i_block[EXT2_IND_BLOCK+1]) != block_size){
-		res = -errno;
-			goto out;
-	}
-
-	for (int j = 0; j < block_size/4; ++j)
-	{
-		if(pread(img, (char*)x1blocks, block_size, block_size * x2blocks[j]) != block_size){
-			res = -errno;
-			goto out;
-		}
-			
-		res = block_transfer(img, out, block_size, &remainfilesize, block_size/4, x1blocks);
-		if(res <= 0)
-			goto out;
-	}
-
-out: free(x1blocks);
-	 free(x2blocks);
-	 return res;
+	return copy_file(img, out, &super_block, block_size, inode_nr);
 }
